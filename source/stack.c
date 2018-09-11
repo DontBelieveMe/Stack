70c45a5ed4c97d1556b0e6538c25a260cb2922fb
Noah Nichols <1101qbtext@gmail.com>
2018-09-11 16:22:31 -0400

    Refactored code for better error reporint in stack.c

--- source/stack.c
+++ source/stack.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int _addp(stack *s, int x, int y) {
+int _addp(Stack *s, int x, int y) {
 	int result = x + y;
 	stack_push(s, result);
 	return result;
@@ -13,25 +13,28 @@ int _addp(stack *s, int x, int y) {
 	Creates a new stack. Free with stack_close(). Returns 0 on failure.
 */
 
-stack*
-new_stack() {
-	stack *_stack = malloc(sizeof(stack));
+error_code
+new_stack(Stack **out) {
+	Stack *new_stack = malloc(sizeof(Stack));
 
-	if(!_stack)
-		return 0;
+	if(!new_stack)
+		return BAD_MEMORY_ALLOC;
 
-	_stack->head = NULL;
-	_stack->size = 0;
-	return _stack;
+	new_stack->head = NULL;
+	new_stack->size = 0;
+	
+	*out = new_stack;
+
+	return OK;
 }
 
 /*
 	Frees all resources belonging to the stack and itself.
 */
 
-void 
-close_stack(stack *s) {
-	element_t *tmp;
+error_code 
+close_stack(Stack *s) {
+	Element *tmp;
 	while(s->head != NULL) {
 		tmp = s->head;
 		s->head = s->head->next;
@@ -41,47 +44,55 @@ close_stack(stack *s) {
 
 	free(s);
 	s = NULL;
+
+	return OK;
 }
 
 /*
 	Pushes a value onto the stack.
 */
 
-void 
-stack_push(stack *s, int value) {
-	element_t *new_element = malloc(sizeof(element_t));
+error_code 
+stack_push(Stack *s, int value) {
+	Element *new_element = malloc(sizeof(Element));
 
 	if(!new_element)
-		return;
+		return BAD_MEMORY_ALLOC;
 
 	new_element->value = value;
 	new_element->next = s->head;
 	s->head = new_element;
 	s->size = s->size + 1;
+
+	return OK;
 }
 
 /*
 	Pops and returns the value on top of the stack.
 */
 
-int 
-stack_pop(stack *s) {
-	int value = -1;
-	element_t *next_node = s->head->next;
-	value = s->head->value;
+error_code
+stack_pop(Stack *s, int *popped_value) {
+
+	if(s->size < 1)
+		return STACK_UNDERFLOW;
+
+	Element *next_node = s->head->next;
+	*popped_value = s->head->value;
 	free(s->head);
 	s->head = next_node;
 	s->size = s->size - 1;
-	return value;
+
+	return OK;
 }
 
 /*
 	Alias for stack_pop().
 */
 
-void 
-stack_popnt(stack *s) {
-	stack_pop(s);
+error_code 
+stack_popnt(Stack *s) {
+	return stack_pop(s, 0);
 }
 
 /*
@@ -89,11 +100,18 @@ stack_popnt(stack *s) {
 	result back onto the stack.
 */
 
-void 
-stack_add(stack *s) {
-	int op1 = stack_pop(s);
-	int op2 = stack_pop(s);
-	_addp(s, op1, op2);
+error_code
+stack_add(Stack *s) {
+	int op;
+	int op2;
+
+	if(stack_pop(s, &op) == STACK_UNDERFLOW || stack_pop(s, &op2) == STACK_UNDERFLOW)
+		return STACK_UNDERFLOW;
+
+	
+	_addp(s, op, op2);
+
+	return OK;
 }
 
 /*
@@ -101,11 +119,17 @@ stack_add(stack *s) {
 	result back onto the stack.
 */
 
-void 
-stack_sub(stack *s) {
-	int op1 = stack_pop(s);
-	int op2 = stack_pop(s);
-	_addp(s, -op1, op2);
+error_code
+stack_sub(Stack *s) {
+	int op;
+	int op2;
+
+	if(stack_pop(s, &op) == STACK_UNDERFLOW || stack_pop(s, &op2) == STACK_UNDERFLOW)
+		return STACK_UNDERFLOW;
+
+	_addp(s, -op, op2);
+
+	return OK;
 }
 
 /*
@@ -113,12 +137,16 @@ stack_sub(stack *s) {
 	result back onto the stack.
 */
 
-void 
-stack_mult(stack *s) {
-	int multip = stack_pop(s);
-	int op     = stack_pop(s);
-	int result = op * multip;
-	stack_push(s, result);
+error_code 
+stack_mult(Stack *s) {
+	int op;
+	int multiplier;
+
+	if(stack_pop(s, &op) == STACK_UNDERFLOW || stack_pop(s, &multiplier) == STACK_UNDERFLOW)
+		return STACK_UNDERFLOW;
+
+	int result = op * multiplier;
+	return stack_push(s, result);
 }
 
 /*
@@ -127,53 +155,96 @@ stack_mult(stack *s) {
 */
 
 void 
-stack_div(stack *s) {
-	int op2 = stack_pop(s);
-	int op1 = stack_pop(s);
-	int result = op1 / op2;
-	stack_push(s, result);
+stack_div(Stack *s) {
+	int op;
+	int op2;
+
+	if(stack_pop(s, &op) == STACK_UNDERFLOW || stack_pop(s, &op2) == STACK_UNDERFLOW)
+		return STACK_UNDERFLOW;
+	
+	int result = op / op2;
+	return stack_push(s, result);
 }
 
 /*
 	Duplicates the item on top of the stack.
 */
 
-void 
-stack_dup(stack *s) {
-	int value = stack_pop(s);
-	stack_push(s, value);
-	stack_push(s, value);
+error_code
+stack_dup(Stack *s) {
+	error_code status_code;
+
+	int value = 0;
+	if((status_code = stack_pop(s, &value) != OK))
+		return status_code;
+	
+	if((status_code = stack_push(s, value) != OK))
+		return status_code;
+	
+	if((status_code = stack_push(s, value)) != OK))
+		return status_code;
+	
+	return OK;
 }
 
 /*
 	Prints each item on the stack.
 */
 
-void
-stack_print(stack *s) {
-	stack_dup(s);
-	printf("%i\n", stack_pop(s));
+error_code
+stack_print(Stack *s) {
+	error_code status_code;
+
+	if((status_code = stack_dup(s)) != OK)
+		return status_code;
+	
+	int value = 0;
+
+	if((status_code = stack_pop(s, &value) != OK))
+		return status_code;
+	
+	return OK;
 }
 
 /*
 	Returns the top item on the stack without popping it.
 */
 
-int 
-stack_gettop(stack *s) {
-	stack_dup(s);
-	int value = stack_pop(s);
-	return value;
+error_code 
+stack_gettop(Stack *s, int *top_stack_value) {
+	error_code status_code;
+
+	if((status_code = stack_dup(s)) != OK)
+		return status_code;
+	
+	int value = 0;
+	if((status_code = stack_pop(s, &value)) != OK)
+		return status_code;
+	
+	return OK;
 }
 
 /*
 	Swap two values on top of the stack.
 */
 
-void 
-stack_swap(stack *s) {
-	int tmp = stack_pop(s);
-	int top = stack_pop(s);
-	stack_push(s, tmp);
-	stack_push(s, top);
+error_code
+stack_swap(Stack *s) {
+	error_code status_code;
+
+	int temp = 0;
+	if((status_code = stack_pop(s, &temp)) != OK)
+		return status_code;
+	
+	int top = 0;
+	if((status_code = stack_pop(s, &top)) != OK)
+		return status_code;
+	
+	if((status_code = stack_push(s, temp)) != OK)
+		return status_code;
+
+	if((status_code = stack_push(s, top)) != OK)
+		return status_code;
+
+	return OK;
 }
